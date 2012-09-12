502a6b6f5a417cf80a74c6301e7be7dea09e5744
AeroNotix <aaron.l.france@gmail.com>
2012-09-12 14:29:43 +0000

    Added a supervisor process for the world. Error checking on double world start/register

--- spacemachine.erl
+++ spacemachine.erl
@@ -3,24 +3,50 @@
 
 start(What, Options) ->
     ID = newid(),
-    Pid = spawn(spacemachine, What, [Options, ID]),
+    Pid = spawn(?MODULE, What, [Options, ID]),
     {ID, Pid}.
 
 %% Models the world and all the objects in it.
 world(Options, MyID, State) ->
-    io:format("Loop~n"),
     receive
 	{newobj, {ID, Pid}} ->
 	    world(Options, MyID, dict:store(ID, Pid, State));
 
 	{reload, Pid, all} ->
-	    spacemachine:world(Options, MyID, State),
 	    Pid ! {reloaded, done},
-	    world(Options, MyID, restart_objects(State));
+	    ?MODULE:world(Options, MyID, restart_objects(State));
 
 	{remove, ID} ->
-	    world(Options, MyID, dict:erase(ID, State))
+	    case dict:find(ID, State) of
+		{ok, Value} ->
+		    Value ! {die, self()},
+		    world(Options, MyID, dict:erase(ID, State));
+		error ->
+		    world(Options, MyID, State)
+	    end;
+
+	{running, Pid} ->
+	    Pid ! {true, running},
+	    world(Options, MyID, State)    
     end.
+
+%% Checks if the world is running and returns a boolean respective of
+%% that.
+running(Who) ->
+    try
+	Who ! {running, self()},
+    receive
+	{true, running} ->
+	    true
+    after
+	1000 ->
+	    false
+    end
+	catch
+	    error:badarg ->
+		  false
+	  end.
+
 
 %% Helper method to restart objects and reload their code.
 restart_objects(Objects) ->
@@ -31,31 +57,59 @@ restart_objects(Objects) ->
 		  {reloaded, done} ->
 		      ok
 	      after
-		  10000 ->
+		  100 ->
 		      error
 	      end
-      end, Objects).
+      end, Objects),
+    Objects.
+
+%% Supervises the world process and restarts the process if required.
+world_supervisor(Options) ->
+
+    %% trap dem exits
+    process_flag(trap_exit, true),
+    Dict = dict:new(),
+    register(world, spawn_link(?MODULE, world, [Options, newid(), Dict])),
+    
+    receive
+	_ ->
+	    io:format("Caught exit~n"),
+	    world_supervisor(Options)	    
+    end.
+    
 
 %% Starts the world
 start_world(Options) ->
-    register(idbroker, spawn(spacemachine, idbroker, [0])),
-    Dict = dict:new(),
-    register(world, spawn(spacemachine, world, [Options, newid(), Dict])).
+    case running(idbroker) of
+	true ->
+	    ok;
+	false ->
+	    register(idbroker, spawn(?MODULE, idbroker, [0]))
+    end,
+    case running(world) of
+	true ->
+	    ok;
+	false ->
+	    spawn(?MODULE, world_supervisor, [Options])
+    end,
+    sleep(1),
+    running(idbroker) and running(world).
 
 %% Creates a new object in the world, allocates it an ID and registers
 %% the ID in the state of the World.
 object(Options, MyID) ->
-    io:format("Loop~n"),
     receive
 	{world, Message} ->
 	    object(Options, MyID);
 	{reload, Pid} ->
 	    Pid ! {reloaded, done},
-	    spacemachine:object(Options, MyID)
+	    ?MODULE:object(Options, MyID)
     end.
 
 register_object({ID, Pid}) ->
     world ! {newobj, {ID, Pid}}.
+unregister_object(ID) ->
+    world ! {remove, ID}.
 
 newobj(Options) ->
     {ID, Pid} = start(object, Options),
@@ -66,7 +120,10 @@ idbroker(N) ->
     receive
 	{newid, Pid} ->
 	    Pid ! {newid, N},
-	    idbroker(N+1)
+	    idbroker(N+1);
+	{running, Pid} ->
+	    Pid ! {true, running},
+	    idbroker(N)
     end.
 
 newid() ->
@@ -76,3 +133,17 @@ newid() ->
 	    ok
     end,
     N.
+
+reset_world() ->
+    world ! {reload, self(), all},
+    receive
+	{reloaded, done} ->
+	    ok
+    end.
+
+sleep(N) ->
+    receive
+    after
+	N ->
+	    ok
+    end.
